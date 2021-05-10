319e0c29c40eeb20f230790497b9863553db875b
Kirill Levchenko <k.levchenko@dodopizza.com>
2021-05-10 09:46:47 +0300

    Add timer run

---
+++ TimerRun/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Threading;
+
+namespace TimerRun
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("I thinking");
+            Thread.Sleep(TimeSpan.FromSeconds(10));
+            Console.WriteLine("I was...");
+        }
+    }
+}
